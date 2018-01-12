8bc95107e07a439e16b3b817101f39dc350a427a
Christopher White <cwhite01+github@gmail.com>
2018-01-12 15:50:44 +0100

    Value objects, Entities, Repositories en Services Layer

--- brouwers/src/main/java/be/vdab/web/BrouwerController.java
+++ brouwers/src/main/java/be/vdab/web/BrouwerController.java
@@ -3,6 +3,9 @@ package be.vdab.web;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import be.vdab.services.BrouwerService;
 
 @Controller
 @RequestMapping("/brouwers")
@@ -10,10 +13,19 @@ class BrouwerController {
 private static final String TOEVOEGEN_VIEW = "brouwers/toevoegen";
 private static final String BEGIN_NAAM_VIEW = "brouwers/beginnaam";
 private static final String BROUWERS_VIEW = "brouwers/brouwers";
-@GetMapping
-String brouwers() {
-return BROUWERS_VIEW;
+private final BrouwerService brouwerService;
+
+BrouwerController(BrouwerService brouwerService) {
+this.brouwerService = brouwerService;
 }
+@GetMapping
+ModelAndView brouwers() {
+return new ModelAndView(BROUWERS_VIEW, "brouwers", brouwerService.findAll());
+}
+//@GetMapping
+//String brouwers() {
+//return BROUWERS_VIEW;
+//}
 @GetMapping("beginnaam")
 String beginNaam() {
 return BEGIN_NAAM_VIEW;
