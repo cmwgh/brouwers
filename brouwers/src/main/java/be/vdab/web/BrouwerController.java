b0ce39c70e837921a8d107651cdb63133a6d945e
Christopher White <cwhite01+github@gmail.com>
2018-01-15 13:57:31 +0100

    2.11 Brouwers op alfabet

--- brouwers/src/main/java/be/vdab/web/BrouwerController.java
+++ brouwers/src/main/java/be/vdab/web/BrouwerController.java
@@ -3,6 +3,7 @@ package be.vdab.web;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import be.vdab.services.BrouwerService;
@@ -14,9 +15,14 @@ private static final String TOEVOEGEN_VIEW = "brouwers/toevoegen";
 private static final String BEGIN_NAAM_VIEW = "brouwers/beginnaam";
 private static final String BROUWERS_VIEW = "brouwers/brouwers";
 private final BrouwerService brouwerService;
+private static final String ALFABET_VIEW = "brouwers/opalfabet";
+private final char[] alfabet = new char['Z' - 'A' + 1];
 
 BrouwerController(BrouwerService brouwerService) {
-this.brouwerService = brouwerService;
+	this.brouwerService = brouwerService;
+	for (char letter = 'A'; letter <= 'Z'; letter++) {
+	alfabet[letter - 'A'] = letter;
+	}
 }
 @GetMapping
 ModelAndView brouwers() {
@@ -33,5 +39,15 @@ return BEGIN_NAAM_VIEW;
 @GetMapping("toevoegen")
 String toevoegen() {
 return TOEVOEGEN_VIEW;
+}
+@GetMapping("opalfabet")
+ModelAndView opAlfabetForm() {
+return new ModelAndView(ALFABET_VIEW, "alfabet", alfabet);
+}
+@GetMapping(params = "letter")
+ModelAndView opAlfabet(@RequestParam char letter) {
+return new ModelAndView(ALFABET_VIEW)
+.addObject("alfabet", alfabet)
+.addObject("brouwers", brouwerService.findByNaam(String.valueOf(letter)));
 }
 }
